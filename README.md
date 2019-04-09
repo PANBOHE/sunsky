744a9f8cea8184ad6fd37040e482f1659edf9540
Panbo <panbohero@outlook.com>
2019-04-09 09:57:34 +0800

    搭建基础项目
    搭建

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sunsky
-springboot+mysql+自己联系
+springboot+mysql+自己练习
+
+先配置基础  
+JDK 1.8 +maven项目----------------2019-4-9
---
+++ sixmonthsky/src/main/java/com/sunsky/sixmonthsky/SixmonthskyApplication.java
@@ -0,0 +1,13 @@
+package com.sunsky.sixmonthsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixmonthskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixmonthskyApplication.class, args);
+	}
+
+}
